003d353117750cf3edb1830cba756f96b4efcdf2
gourabxz <gourabxz@gmail.com>
2019-01-16 00:24:17 +0600

    downlading ok

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var os = require('os');
 var http = require('http'),
     url = require('url'),
     fs = require('fs');
+var bodyParser = require('body-parser');
+
 
 var app = express();
 
@@ -13,46 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 
 var cb0 = function (req, res, next) {
-  console.log('DOWNLOAD STARTED');
-  var dir = os.tmpdir()
-  var YD = new YoutubeMp3Downloader({
-      "ffmpegPath": ffmpeg.path,        // Where is the FFmpeg binary located?
-      "outputPath": dir,    // Where should the downloaded and encoded files be stored?
-      "youtubeVideoQuality": "highest",       // What video quality should be used?
-      "queueParallelism": 2,                  // How many parallel downloads/encodes should be started?
-      "progressTimeout": 2000                 // How long should be the interval of the progress reports
-  });
-
-  YD.download("https://www.youtube.com/watch?v=ApXoWvfEYVU");
-
-  YD.on("finished", function(err, data) {
-
-      var filePath = path.join(os.tmpdir(), data.videoTitle);
-      console.log(JSON.stringify(filePath));
-
-      var query = url.parse(req.url, true).query;
-      fs.readFile(filePath + ".mp3", function (err, content) {
-        if (err) {
-            res.writeHead(400, {'Content-type':'text/html'})
-            console.log(err);
-            res.end("No such file");    
-        } else {
-            //specify Content will be an attachment
-            res.setHeader('Content-disposition', 'attachment; filename='+data.videoTitle + ".mp3");
-            res.end(content);
-        }
-      });
-
-      
-  });
-   
-  YD.on("error", function(error) {
-      console.log(error);
-  });
-   
-  YD.on("progress", function(progress) {
-      console.log(JSON.stringify(progress));
-  });
+    res.redirect('/');
 }
 
 var cb1 = function (req, res, next) {
@@ -61,14 +24,70 @@ var cb1 = function (req, res, next) {
 }
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.get('/', [cb0, cb1], (req, res) => res.render('pages/index',{
+app.get('/', [cb1], (req, res) => res.render('pages/index',{
       title:  os.type()+os.arch(),
       ffmpegPath: ffmpeg.path
     }));
+app.get('/download', (req, res) => res.render('pages/index'));
 
 
+app.post('/download', function(req, res, next){
+    var linkData = req.body.urlData;
+    console.log(linkData);
+    console.log('DOWNLOAD STARTED');
+    var dir = os.tmpdir()
+    var YD = new YoutubeMp3Downloader({
+        "ffmpegPath": ffmpeg.path,        // Where is the FFmpeg binary located?
+        "outputPath": dir,    // Where should the downloaded and encoded files be stored?
+        "youtubeVideoQuality": "highest",       // What video quality should be used?
+        "queueParallelism": 2,                  // How many parallel downloads/encodes should be started?
+        "progressTimeout": 2000                 // How long should be the interval of the progress reports
+    });
+    
+    YD.download(linkData);
+  
+    YD.on("finished", function(err, data) {
+  
+        var filePath = path.join(os.tmpdir(), data.videoTitle);
+        console.log(JSON.stringify(filePath));
+  
+        //var query = url.parse(req.url, true).query;
+        fs.readFile(filePath + ".mp3", function (err, content) {
+          if (err) {
+              res.writeHead(400, {'Content-type':'text/html'})
+              console.log(err);
+              res.end("No such file");    
+          } else {
+              var j = "zero";
+              //specify Content will be an attachment
+              if(z="zero"){
+                res.setHeader('Content-disposition', 'attachment; filename='+data.videoTitle + ".mp3");
+                res.send(content);
+                z="hero";
+              }else{
+                res.redirect('/');
+              }
+              
+              
+          }
+        });
+  
+        
+    });
+     
+    YD.on("error", function(error) {
+        console.log(error);
+    });
+     
+    YD.on("progress", function(progress) {
+        console.log(JSON.stringify(progress));
+    });
+    
+})
 
 
 
