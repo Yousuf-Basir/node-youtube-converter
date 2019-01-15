43c833b435f8165c43657bd66da5f24ffa3ab0a9
gourabxz <gourabxz@gmail.com>
2019-01-15 18:26:40 +0600

    os info

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var os = require('os');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('pages/index',{
+    title:  os.type()+os.arch() 
+  }))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 // const express = require('express')
---
+++ views/pages/my.js
@@ -0,0 +1,2 @@
+var os = require('os');
+console.log(os.arch());
